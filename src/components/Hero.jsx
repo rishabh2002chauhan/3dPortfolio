7d84568424fc1b025550f59ed89a5a6f17ca6e25
Rishabh Chauhan <dr.smolderrishabhchauhan@gmail.com>
2024-05-27 20:22:18 +0530

    Updated Hero section and added about, experience, tech, works and feedback section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto bg-hero-pattern bg-center'>
+    <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
@@ -14,7 +14,15 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Rishabh</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I like to make websites <br className='sm:block hidden'/>and user interfaces.</p>
         </div>
-        
+      </div>
+      <ComputersCanvas />
+
+      <div className='absolute xs: bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div animate={{ y: [0, 24, 0]}} transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop'}} className='w-3 h-3 rounded-full bg-secondary mb-1' />
+          </div>
+        </a>
       </div>
     </section>
   )
